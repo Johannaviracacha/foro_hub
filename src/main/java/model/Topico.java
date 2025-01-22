b7e20fcb824401b87bfff6eb2dd3d1776ba9ca3f
Johannaviracacha <johannaviracacha@gmail.com>
2025-01-21 20:36:14 -0500

    Mi primer commit

---
+++ src/main/java/model/Topico.java
@@ -0,0 +1,77 @@
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Topico {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String mensaje;
+
+    @NotBlank
+    private String autor;
+
+    @NotBlank
+    private String curso;
+
+    @Column(nullable = false, updatable = false)
+    private java.time.LocalDateTime fechaCreacion = java.time.LocalDateTime.now();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+// Getters y Setters
+}
